4e1150b5068908d4369b1d263f356831ab3afd20
Ondrej Sykora <mail@ondrejsykora.com>
2019-06-15 11:14:43 +0200

    Completed the lightbox.

--- public/script.js
+++ public/script.js
@@ -1,3 +1,30 @@
 function hideLightbox(element) {
   element.style.display = 'none';
 }
+
+function showInLightbox(imageUrl) {
+  const lightbox = document.getElementById('lightbox');
+  const imgElement = document.getElementById('lightbox-image');
+
+  lightbox.style.display = 'block';
+  imgElement.src = imageUrl;
+}
+
+function onKeyPressInLightbox(event) {
+  const lightbox = document.getElementById('lightbox');
+  if (event.code == 'Escape' && lightbox.style.display != 'none') {
+    hideLightbox(lightbox)
+  }
+}
+document.addEventListener('keyup', onKeyPressInLightbox);
+
+function decorateImageLinks() {
+  const imageLinks = document.querySelectorAll('.images a');
+  for (var i = 0; i < imageLinks.length; i++) {
+    const link = imageLinks.item(i);
+    link.onclick = function() {
+      showInLightbox(link.href);
+      return false;
+    }
+  }
+}
